52fed47f71d20720c0ddd06b8f5c19ae5f01010a
Tate Mouser <tatesmouser@gmail.com>
2023-08-03 18:55:54 -0400

    Comparing 2 Tree Nodes

--- CodePractice/src/main/Index.java
+++ CodePractice/src/main/Index.java
@@ -1,12 +1,29 @@
 package main;
-import java.util.List;
 
 import dataTypes.*;
 
 public class Index {
-	public static void main(String[] args) {
-		CharStack temp = new CharStack();
-		System.out.println(temp.test("()()"));
-		System.out.println(temp.test("()()("));
-	}
+    public static void main(String[] args) {
+        // Create two binary trees for testing
+        TreeNode tree1 = new TreeNode(1);
+        tree1.left = new TreeNode(2);
+        tree1.right = new TreeNode(3);
+
+        TreeNode tree2 = new TreeNode(1);
+        tree2.left = new TreeNode(2);
+        tree2.right = new TreeNode(3);
+
+        // Create an instance of CompareTreeNode to test the isSameTree method
+        CompareTreeNode compareTreeNode = new CompareTreeNode();
+
+        // Test the isSameTree method
+        boolean areSame = compareTreeNode.isSameTree(tree1, tree2);
+
+        // Display the result
+        if (areSame) {
+            System.out.println("The two binary trees are the same.");
+        } else {
+            System.out.println("The two binary trees are NOT the same.");
+        }
+    }
 }
