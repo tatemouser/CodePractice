2d835403b2af51ddd8c68d78ee241d0dd57c9b7c
Tate Mouser <tatesmouser@gmail.com>
2023-08-03 18:56:14 -0400

    Comparing 2 Tree Nodes

---
+++ CodePractice/src/dataTypes/CompareTreeNode.java
@@ -0,0 +1,24 @@
+package dataTypes;
+
+import main.*;
+
+public class CompareTreeNode {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if (p == null || q == null) {
+            return false;
+        }
+
+        if (p.val != q.val) {
+            return false;
+        }
+
+        boolean isLeftSame = isSameTree(p.left, q.left);
+        boolean isRightSame = isSameTree(p.right, q.right);
+
+        return isLeftSame && isRightSame;
+    }
+}
