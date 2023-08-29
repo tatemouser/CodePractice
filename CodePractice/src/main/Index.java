c9ffaa18f6cd7dd1ea99371cabdbdc9892aea90a
Tate Mouser <tatesmouser@gmail.com>
2023-08-29 12:01:18 -0500

    Test

--- CodePractice/src/main/Index.java
+++ CodePractice/src/main/Index.java
@@ -4,9 +4,5 @@ import dataTypes.*;
 
 public class Index {
     public static void main(String[] args) {
-    	LargestIntPossibility sol = new LargestIntPossibility();
-    	int num = 6996;
-    	System.out.println(sol.maxNum(num));
- 
     }
 }
